d4544afb1efda8e5a6386bbb39d0059d494342fd
zhifeng hu <zf@ancientrocklab.com>
2013-06-15 15:32:50 +0800

    update binary auto

--- compile.sh
+++ compile.sh
@@ -4,8 +4,12 @@ hg pull -f
 sed -i 's/CROSS=no/CROSS=yes/g' src/Make_ming.mak
 sed -i 's/i586-pc-mingw32msvc-/i686-pc-mingw32-/g' src/Make_ming.mak 
 cd src 
+rm -rf *.exe
+make clean
+make distclean
 mingw32-make -f Make_ming.mak
-\cp *.exe /usr/local/src/vim-zip/vim/vim73/
+\cp -rf *.exe /usr/local/src/vim-zip/vim/vim73/ 
 cd /usr/local/src/vim-zip
 zip -r vim.zip vim
+unlink /var/www/vimcustom/vim.zip
 mv vim.zip /var/www/vimcustom
--- update-vim-linux.sh
+++ update-vim-linux.sh
@@ -3,4 +3,5 @@ cd /root/.vim/bundle
 ./update.sh
 cd /root/
 tar zcvf vim.tgz .vim*
+unlink /var/www/vimcustom/vim.tgz
 mv vim.tgz /var/www/vimcustom
