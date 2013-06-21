b9d0aaea7cf5193c6bde99297abe64d5fbd4ff64
zhifeng hu <zf@ancientrocklab.com>
2013-06-21 22:16:59 +0800

    compile the latest vim

---
+++ compile-for-linux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd /usr/local/src
+if [ ! -d "vim" ]; then
+hg clone https://vim.googlecode.com/hg/ vim
+fi
+cd vim
+hg checkout -C
+hg pull -uf
+sed -i 's/#CONF_OPT_GUI = --disable-gui/CONF_OPT_GUI = --disable-gui/g' src/Makefile
+sed -i 's/#CONF_OPT_X = --without-x/CONF_OPT_X = --without-x/g' src/Makefile
+sed -i 's/#CONF_OPT_PYTHON = --enable-pythoninterp=dynamic/CONF_OPT_PYTHON = --enable-pythoninterp=dynamic/g' src/Makefile
+make clean
+make distclean
+export CFLAGS="-O3 -msse -msse2 -mmmx -funroll-loops -mfpmath=sse"
+make && make install
+
+
+
